3c57dc04be56a0c2b8f9407f284997e2633d2bbc
Tahiel Devoto <tahielmartind16@gmail.com>
2023-07-20 16:18:34 -0300

    Divison de las paginas y  landing page

---
+++ MODULO 2 DE VERDAD/SITIO CUBERS/script.js
@@ -0,0 +1,13 @@
+// let categorias_div= document.getElementsByClassName('item_lista');
+// const botones_cat=document.getElementsByClassName('botones-cat');
+
+// botones_cat.addEventListener('click',aparecer());
+// function aparecer(categorias_div){
+//     if(categorias_div.style.display=='none'){
+//         botones_cat.innerText='ver menos'
+//         categorias_div.style.display='block'
+//     }else{
+//         botones_cat.innerText='ver mas'
+//         categorias_div.style.display='none'
+//     }
+// }
